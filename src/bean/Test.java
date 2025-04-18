daa0e280e847bd2037d7de755672483aecba2501
aiueo-iikibun <tky2402135@stu.o-hara.ac.jp>
2025-04-18 12:38:16 +0900

    a

---
+++ src/bean/Test.java
@@ -0,0 +1,59 @@
+package bean;
+
+public class Test {
+	private Student student;
+	private String classNum;
+	private School school;
+	private Subject subject;
+
+	private int no;
+	private int point;
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public String getClassNum() {
+		return classNum;
+	}
+
+	public void setClassNum(String classNum) {
+		this.classNum = classNum;
+	}
+
+	public School getSchool() {
+		return school;
+	}
+
+	public void setSchool(School school) {
+		this.school = school;
+	}
+
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+}
